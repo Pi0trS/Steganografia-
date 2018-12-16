9060d9c3a661d8a91a881c56382f4436e83455c2
Pi0trS <piotrsy82@gmail.com>
2018-12-16 22:03:37 +0100

    :/

---
+++ Steganografia/Modle/ImageHolder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Steganografia.Modle
+{
+    class ImageHolder
+    {
+        private Image image;
+
+        public ImageHolder(string path)
+        {
+            setImage(path);
+        }
+
+        void setImage(string path)
+        {
+            image = Image.FromFile(path);
+        }
+        Image getImage()
+        {
+
+        }
+    }
+}
