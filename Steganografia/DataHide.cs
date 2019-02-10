e72c6d224b22d2cecb3fe14c7a95ee5e4bdddfb6
Piotr <piotrsy82@gmail.com>
2019-02-10 12:25:09 +0100

    hiderInformationStart

---
+++ Steganografia/DataHide.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Steganografia
+{
+    class DataHide
+    {
+        static Image hideInformation(Image oldImage, Byte[] data, string password)
+        {
+            Image encryptedImagae =new Bitmap(oldImage);
+            SHA512 sha = SHA512.Create();
+            Byte[] key = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            return encryptedImagae;
+        }
+    }
+}
