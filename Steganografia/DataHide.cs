619ab551b25642fa9d79cea663ffd1ecabfdf319
Pi0trS <piotrsy82@gmail.com>
2019-03-07 23:02:02 +0100

    Crypto

--- Steganografia/DataHide.cs
+++ Steganografia/DataHide.cs
@@ -127,6 +127,82 @@ namespace Steganografia
             return decodeMessage;
         }
 
+        public static String encrypt(String mesage, String password)
+        {
+            var bytesToencrypt = Encoding.UTF8.GetBytes(mesage);
+            var passwordInBytes = Encoding.UTF8.GetBytes(password);
+
+            passwordInBytes = SHA256.Create().ComputeHash(passwordInBytes);
+            var encryptedBytes = encryptB(bytesToencrypt, passwordInBytes);
+            return Convert.ToBase64String(encryptedBytes);
+
+        }
+
+        public static String decrypt(String mesageToDecrypt, String password)
+        {
+            var bytesToDecrypt = Convert.FromBase64String(mesageToDecrypt);
+            var passwordInBytes = Encoding.UTF8.GetBytes(password);
+            passwordInBytes = SHA256.Create().ComputeHash(passwordInBytes);
+
+        var bytesDecrypted = decryptB(bytesToDecrypt, passwordInBytes);
+
+            return Encoding.UTF8.GetString(bytesDecrypted);
+        }
+
+        public static byte[] encryptB(byte[] byteToEncrypt, byte[] password)
+        {
+            byte[] encryptedB = null;
+            var saltB = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    var key = new Rfc2898DeriveBytes(password, saltB, 1000);
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(byteToEncrypt, 0, byteToEncrypt.Length);
+                        cs.Close();
+                    }
+                    encryptedB = ms.ToArray();
+                }
+                return encryptedB;
+            }
+        }
+
+        public static byte[] decryptB(byte[] byteToDecrypt, byte[] password)
+        {
+            byte[] decryptedB = null;
+            var saltB = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    var key = new Rfc2898DeriveBytes(password, saltB, 1000);
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(byteToDecrypt, 0, byteToDecrypt.Length);
+                        cs.Close();
+                    }
+                    decryptedB = ms.ToArray();
+                }
+                return decryptedB;
+            }
+        }
+        /*
         public static Image hideInformationNoWork(Bitmap oldImage, string data, string password)
         {
             Bitmap encryptedImage = new Bitmap(oldImage);
@@ -361,6 +437,6 @@ namespace Steganografia
                 }
             }
         }
-    }
+    */}
 }
 
